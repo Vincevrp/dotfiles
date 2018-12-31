2f3a637697d911645ddc620e87fcf102ce3261d2
Vincevrp <vince@vince.sh>
2018-12-31 01:27:15 +0100

    Remove unnecessary check from up function

--- .shell/functions.sh
+++ .shell/functions.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-function up()
-{
-    AMOUNT=$1
+function up() {
+    AMOUNT=${1:-1}
     D=$PWD
-
-    if [[ -z $1 ]]; then
-        AMOUNT=1
-    fi
 
     for ((i=1; i <= AMOUNT; i++)); do
         D=$D/..
