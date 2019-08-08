4acc9a528dea080d71cb1f2cccd0afff2b0f239f
Vincevrp <vince@vince.sh>
2019-08-09 00:54:30 +0200

    Add Python bins to PATH

--- .config/shell/vars.sh
+++ .config/shell/vars.sh
@@ -4,6 +4,7 @@
 export PATH=$PATH:~/.bin                    # Add ~/.bin to PATH
 export PATH=$PATH:~/.gem/ruby/2.6.0/bin     # Add ruby gems to PATH
 export PATH=$PATH:~/.npm/node_modules/bin   # Add node_modules to PATH
+export PATH=$PATH:~/.local/bin              # Add python bins to PATH
 
 # General
 export EDITOR=/usr/bin/vim
