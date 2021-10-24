d4b094c13b3fa1375fca87d8a4c7d3ee7a7d6eac
Vincevrp <vince@vince.sh>
2021-10-24 21:09:08 +0200

    autorandr: Implement initial autorandr config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,14 @@ killall -q polybar
 while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 0.1; done
 
 # Launch bars
-polybar main &
-polybar secondary &
+if [ "$AUTORANDR_CURRENT_PROFILE" = 'mobile' ]; then
+    MONITOR="eDP1"  polybar main &
+    MONITOR="HDMI-1" polybar secondary &
+    MONITOR="HDMI-2" polybar secondary &
+else
+    MONITOR="DP1-1" polybar main &
+    MONITOR="DP1-2" polybar secondary &
+    MONITOR="DP1-3" polybar secondary &
+fi
 
 echo "Bars launched..."
