34667fdb9dab3398c2c0273c6c86396311679fc0
Vincevrp <vince@vince.sh>
2022-07-31 12:36:48 +0200

    Add latitude overrides

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,7 +9,14 @@ while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 0.1; done
 # Launch bars
 polybar main &
 
-MONITOR=HDMI-0 polybar secondary &
-MONITOR=DVI-D-0 polybar secondary &
+ if [ "$AUTORANDR_CURRENT_PROFILE" = 'mobile' ]; then
+    MONITOR="eDP1"  polybar main &
+    MONITOR="HDMI-1" polybar secondary &
+    MONITOR="HDMI-2" polybar secondary &
+else
+    MONITOR="DP-1-1" polybar main &
+    MONITOR="DP-1-2" polybar secondary &
+    MONITOR="DP-1-3" polybar secondary &
+fi
 
 echo "Bars launched..."
